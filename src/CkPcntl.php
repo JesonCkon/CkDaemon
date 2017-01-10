fb18767b49331c84a579bf694e2cbbee15564bc3
Jeson.ckon <ckontem@gmail.com>
2017-01-10 17:00:09 +0800

    upgrade lib

--- src/CkPcntl.php
+++ src/CkPcntl.php
@@ -11,13 +11,27 @@ namespace CkDaemon;
 
 class CkPcntl
 {
+    private $pid_dirs = array();
+    private $processName = '';
     private $isAble = false;
     private $jobs = array();
     public $jobs_return = array();
+    public $isDaemon = false;
     private $php_cli_version = '';
+    private $argv;
+    private $argc;
+    const _DS = DIRECTORY_SEPARATOR;
 
     public function __construct()
     {
+        $this->argv = $GLOBALS[ "argv" ];
+        $this->argc = $GLOBALS[ "argc" ];
+        $run_script_name = $this->argv[ 0 ];
+        $sys_default_pid_dir = dirname($run_script_name);
+        if (!in_array($sys_default_pid_dir, $this->pid_dirs)) {
+            array_push($this->pid_dirs, $sys_default_pid_dir);
+            $this->processName = md5($run_script_name);
+        }
         if (function_exists("pcntl_fork")) {
             $this->isAble = true;
         }
@@ -25,6 +39,7 @@ class CkPcntl
         if ($version_passed == false) {
             die("PHP version must be > 5.4.0");
         }
+
         $this->php_cli_version = $php_version;
     }
 
@@ -36,8 +51,9 @@ class CkPcntl
         }
     }
 
-    public function forkWait($is_deamon = false)
+    private function forkWait()
     {
+        $is_deamon = $this->isDaemon;
         declare(ticks = 1);
         if ($this->isAble == false) {
             die("pcntl model error or not exist!");
@@ -53,6 +69,8 @@ class CkPcntl
             pcntl_wait($status); //等待子进程中断，防止子进程成为僵尸进程。
         } else {
             echo "子进程running  " . getmypid();
+            $pid = getmypid();
+            $this->registerPidFile($pid);
             $this->registerSignalHandler();
             declare (ticks = 1);
             if ($is_deamon == true) {
@@ -62,7 +80,9 @@ class CkPcntl
             } else {
                 $this->doExecute();
             }
+            exit(0);
         }
+        exit(0);
     }
 
     function signalHandler($signal)
@@ -101,5 +121,88 @@ class CkPcntl
         pcntl_signal(SIGTERM, array($this, 'signalHandler'));
         pcntl_signal(SIGUSR1, array($this, 'signalHandler'));
         pcntl_signal(SIGUSR2, array($this, 'signalHandler'));
+    }
+
+    protected function registerPidFile($pid)
+    {
+        $pid_file = $this->createPidFile();
+        if ($pid_file != false) {
+            file_put_contents($pid_file, $pid);
+        }
+    }
+
+    protected function createPidFile()
+    {
+        $default_dir = $this->pid_dirs[ 0 ];
+        if (is_dir($default_dir) && is_writable($default_dir)) {
+            $pid_file = $default_dir . self::_DS . $this->processName . ".pid";
+        } else {
+            return false;
+        }
+
+        return $pid_file;
+    }
+
+    protected static function getPidByFile($filename)
+    {
+        if (file_exists($filename)) {
+            return intval(file_get_contents($filename));
+        } else {
+            return 0;
+        }
+    }
+
+    public function stop()
+    {
+        $pid_file = $this->createPidFile();
+        $pid = self::getPidByFile($pid_file);
+        if ($pid > 0) {
+            posix_kill($pid, SIGTERM);
+        }
+    }
+    public function status()
+    {
+        $pid_file = $this->createPidFile();
+        $pid = self::getPidByFile($pid_file);
+        if ($pid > 0) {
+            $is_running = posix_getpgid($pid);
+            if($is_running == true){
+                echo "进程PID : ".$pid." is Running\n";
+            }
+        }
+    }
+
+    public function setProcessName($name)
+    {
+        $this->processName = $name;
+    }
+
+    public function start()
+    {
+        $this->forkWait();
+    }
+
+    protected function usage()
+    {
+        printf("Usage: %s {start | stop | restart | status}\n", $this->argv[ 0 ]);
+    }
+
+    public function run()
+    {
+        if ($this->argc != 2) {
+            $this->usage();
+        } else {
+            if ($this->argv[ 1 ] == 'start') {
+                $this->start();
+            } else if ($this->argv[ 1 ] == 'stop') {
+                $this->stop();
+            } else if ($this->argv[ 1 ] == 'restart') {
+                $this->restart();
+            } else if ($this->argv[ 1 ] == 'status') {
+                $this->status();
+            } else {
+                $this->usage();
+            }
+        }
     }
 }
